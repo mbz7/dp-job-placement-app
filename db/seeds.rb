d06b08104ee1d5079e4c30fd6c52f051c3d2a4fe
Myles <bracken.myles@gmail.com>
2022-06-30 13:57:20 -0700

    rails api seed data setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# start seeding
+puts '🌱 Seeding data...'
+
+puts '🗑 Destroying old data...'
+Client.destroy_all
+Job.destroy_all
+Candidate.destroy_all
+
+# create companies
+puts 'Creating Clients...'
+c1 =
+  Client.create!(
+    user_id: "1",
+    company_name: Faker::Company.name,
+  )
+
+c2 =
+  Client.create!(
+    user_id: "2",
+    company_name: Faker::Company.name,
+  )
+
+c3 =
+  Client.create!(
+    user_id: "1",
+    company_name: Faker::Company.name,
+  )
+
+# create jobs
+puts 'Creating jobs...'
+j1 = Job.create!(
+    client_id: c1.id,
+    contact: 'Maria Rodriguez',
+    email: 'M.RD43@datapiper.com'
+    role: 'Core Senior Software Developer - Senior Associate',
+    urgency: 'High',
+    quantity: '1',
+    skills: 'Java, Springboot Docker, Kubernetes, API, Microservices, Cloud',
+)
+
+# create candidates
+puts 'Creating candidates...'
+
+3.times do
+Candidate.create!(
+    job_id: j1.id,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone: Faker::PhoneNumber.phone_number,
+    city: 'Los Angeles',
+    state: 'CA',
+)
+end

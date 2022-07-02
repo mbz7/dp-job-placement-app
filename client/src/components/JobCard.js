eb3aaa1dc04481fd14eb96e0edc028efc9ee5a02
Myles <bracken.myles@gmail.com>
2022-07-02 00:08:20 -0700

    new card styling

--- client/src/components/JobCard.js
+++ client/src/components/JobCard.js
@@ -9,6 +9,8 @@ import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
 import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
+import { PersonCircle } from "react-bootstrap-icons";
+import { Envelope } from "react-bootstrap-icons";
 
 function JobCard({
   id,
@@ -28,21 +30,22 @@ function JobCard({
       {/* <div className="job-card-container"></div> */}
       {/* <Card className="p-4 mb-4 bg-light rounded shadow-sm"> */}
       <div className="job-card-container">
-        <Row>
-          <Col lg={6} className="job-card-title-group">
-            <h3 className="job-card-job_title">{role}</h3>
-            <p className="job-card-client_name">{client} </p>
-          </Col>
-          <Col className="p-4 d-flex align-items-center justify-content-center">
-            <Button
-              className="job-card-view-button"
-              as={Link}
-              to={`/jobs/${id}`}
-            >
-              <span>Add / View Candidates</span>
-            </Button>
-          </Col>
-          {/* <Col
+        <div className="job-card-container-content">
+          <Row>
+            <Col lg={6} className="job-card-title-group">
+              <h3 className="job-card-job_title">{role}</h3>
+              <p className="job-card-client_name">{client} </p>
+            </Col>
+            <Col className="p-4 d-flex align-items-center justify-content-center">
+              <Button
+                className="job-card-view-button"
+                as={Link}
+                to={`/jobs/${id}`}
+              >
+                <span>Add / View Candidates</span>
+              </Button>
+            </Col>
+            {/* <Col
                   lg={3}
                   className="d-flex align-items-start justify-content-end"
                 >
@@ -50,28 +53,28 @@ function JobCard({
                     <Priority urgency={urgency} />
                   </div>
                 </Col> */}
-        </Row>
-        <hr />
-        <Row className="d-flex flex-row gap-3">
-          <p className="job-card-skills">SKILLS</p>
-          <Col lg={8} className="gap-3 mr-5">
-            <div>
-              {skillsArry.map((skill, id) => {
-                return (
-                  <Button
-                    className="job-card-skills-button m-2 text-dark"
-                    // size="sm"
-                    // variant="light"
-                    key={id}
-                  >
-                    <span>{skill}</span>
-                  </Button>
-                );
-              })}
-            </div>
-          </Col>
-          <Col lg={3} className="d-flex flex-row-reverse align-right">
-            {/* <Col lg={3}>
+          </Row>
+          <hr />
+          <Row className="d-flex flex-row">
+            <p className="job-card-subheader">SKILLS</p>
+            <Col lg={8} className="gap-3 mr-5">
+              <div>
+                {skillsArry.map((skill, id) => {
+                  return (
+                    <Button
+                      className="job-card-skills-button m-2 text-dark"
+                      // size="sm"
+                      // variant="light"
+                      key={id}
+                    >
+                      <span>{skill}</span>
+                    </Button>
+                  );
+                })}
+              </div>
+            </Col>
+            <Col lg={3} className="d-flex flex-row-reverse align-right">
+              {/* <Col lg={3}>
                     <p>{client} </p>
                   </Col>{" "}
                   <Col lg={3}>
@@ -80,28 +83,50 @@ function JobCard({
                   <Col lg={4}>
                     <p>Email: {email}</p>
                   </Col>{" "} */}
-            <Col className="text-center">
-              <p className="job-card-quantity">Quanity</p>
-              <p className="job-card-quantity-number"> {quantity}</p>
+              <Col className="text-center">
+                <p className="job-card-quantity">Quanity</p>
+                <p className="job-card-quantity-number"> {quantity}</p>
+              </Col>
+              <Col className="text-center">
+                <p className="job-card-quantity">Urgency</p>
+                <div>
+                  <Priority urgency={urgency} />
+                </div>
+              </Col>{" "}
             </Col>
-            <Col className="text-center">
-              <p className="job-card-quantity">Urgency</p>
-              <div>
-                <Priority urgency={urgency} />
-              </div>
-            </Col>{" "}
-          </Col>
-        </Row>{" "}
-        <hr />
-        {/* <strong>Client:</strong>  */}
-        <Button
+          </Row>{" "}
+          <hr />
+          <p className="job-card-subheader">CONTACT</p>
+          <Container>
+            <Row className="w-75">
+              <Col lg={6} sm={6} className="d-flex flex-row gap-3">
+                <Col lg={1}>
+                  <PersonCircle size={30} />
+                </Col>
+                <Col>
+                  <p className="job-card-contact-text">{contact}</p>
+                </Col>
+              </Col>
+              <Col lg={6} sm={6} className="d-flex flex-row gap-3">
+                <Col lg={1}>
+                  <Envelope size={30} />
+                </Col>
+                <Col>
+                  <p className="job-card-contact-text">{email}</p>
+                </Col>
+              </Col>
+            </Row>
+          </Container>
+          {/* <strong>Client:</strong>  */}
+          {/* <Button
           className="text-right mt-2 mb-1 btn-lg"
           as={Link}
           to={`/jobs/${id}`}
           variant="dark"
         >
           Add / View Candidates
-        </Button>
+        </Button> */}
+        </div>
       </div>
 
       {/* <Container>
